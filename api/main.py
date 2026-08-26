fe0dd272ac55d28b30bd73dc461fb31b294e6832
Pham Quoc Nghiep <pqnghiep1354@gmail.com>
2026-08-26 20:51:08 +0700

    chore: remove quarter-plan and produce admin pages [AA-467]

--- api/main.py
+++ api/main.py
@@ -41,7 +41,6 @@ from api.routers.admin_acp_proxy import router as admin_acp_proxy_router
 from api.routers.admin_settings import router as admin_settings_router
 from api.routers.admin_atoms import router as admin_atoms_router
 from api.routers.admin_marketplace import router as admin_marketplace_router
-from api.routers.admin_produce import router as admin_produce_router
 from api.routers.admin_a4 import router as admin_a4_router
 from api.routers.acp_health import router as acp_health_router
 from api.middleware.rate_limit import rate_limit_middleware
@@ -140,7 +139,6 @@ app.include_router(admin_acp_proxy_router)
 app.include_router(admin_settings_router)
 app.include_router(admin_atoms_router)
 app.include_router(admin_marketplace_router)
-app.include_router(admin_produce_router)
 app.include_router(admin_a4_router)
 app.include_router(acp_health_router)
 
